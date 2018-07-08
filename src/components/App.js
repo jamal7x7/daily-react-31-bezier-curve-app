1ace6b03ab6baa1b80cdaaaa5d943bfd158c593d
jamal7X7 <jamal7x7@gmail.com>
2018-07-08 02:10:20 +0100

    playing with clientX,Y

--- src/components/App.js
+++ src/components/App.js
@@ -8,18 +8,28 @@ const Header = () => (
 )
 
 const Items = (props) => (
-  <div className=' artboard '>
+  <div className=' artboard ' onMouseMove={props.handleMouseMove} onClick={props.handleClick}>
 
-    <svg height= '300' width= '700' >
+    <svg className='svg' viewBox='0 0 1000 700'  height= '700' width= '1000' >
+
       <path d='M 10 20 C 100 150 200 200 130 280 ' 
         stroke='pink' 
         fill='none' 
       />
+
+      <path d='M 10 20 L 100 150  ' 
+        stroke='white' 
+        fill='none' 
+      />
+
       <circle cx='100' cy='150' r='5' fill='purple'/>
       <circle cx='200' cy='200' r='5' fill='purple'/>
       <circle cx='130' cy='280' r='5' fill='purple'/>
 
-      {/* <polygon points={'50,300 300,10 650,300'} /> */}
+      { (props.c.length > 0) && <circle cx={props.c[0][0]} cy={props.c[0][1]} r='5' fill='violet'/>}
+      
+      
+      
     </svg>
   </div>
 )
@@ -37,19 +47,46 @@ const Menu = () => (
 class BezierCurveApp extends Component {
   
   state = {
-    
-    currentPrice: 6677,
-    points: []
+    pointX: 0,
+    c: []
   }
-
   
 
-  componentDidMount() {
-     
+
+  handleMouseMove = (e) => {
+    e.persist()
+    // console.log(
+    //   'onMouseMove',
+    //   e.nativeEvent.clientX, e.nativeEvent.clientY
+    // )
+
+    const svg = document.body.querySelector('svg')
+    const rect = svg.getBoundingClientRect()
+
+    let x = e.nativeEvent.clientX - rect.left
+    let y = e.nativeEvent.clientY - rect.top
+    this.setState( (prevState) =>  ({
+            c: [
+              [x, y]
+            ]
+          })
+    )
     
+    
+    // this.updateCState (this, e)
   }
 
- 
+  componentDidMount() {
+    const svg = document.body.querySelector('svg')
+    const rect = svg.getBoundingClientRect();
+    console.log(rect.top, rect.right, rect.bottom, rect.left);
+
+  }
+
+  handleClick (e) {
+    console.log('click')
+  }
+
 
   render () {
     return (
@@ -58,7 +95,9 @@ class BezierCurveApp extends Component {
 
         <Menu />
         <Items 
-       
+          c={this.state.c}
+          handleMouseMove={this.handleMouseMove}
+          handleClick={this.handleClick}
         />
 
       </div>
